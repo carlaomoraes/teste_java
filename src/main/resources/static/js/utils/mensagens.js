5917a9acad01eb40ed2fa6bd0b21a6d27df0d503
carlaomoraes <carlaomoraes@hotmail.com>
2026-07-18 22:11:23 -0300

    Mensagens padronizadas

---
+++ src/main/resources/static/js/utils/mensagens.js
@@ -0,0 +1,20 @@
+const COR = {
+    SUCESSO: "#198754",
+    ERRO: "#dc3545",
+    ALERTA: "#ffc107",
+    INFO: "#0dcaf0",
+
+    PROGRESSO_100: "#198754",
+    PROGRESSO_75: "#20c997",
+    PROGRESSO_50: "#ffc107",
+    PROGRESSO_0: "#dc3545"
+};
+const MSG = {
+    salvo: (nome) => `${nome} salvo(a) com sucesso.`,
+    alterado: (nome) => `${nome} alterado(a) com sucesso.`,
+    excluido: (nome) => `${nome} excluído(a) com sucesso.`,
+    erroSalvar: (nome) => `Erro ao salvar ${nome}.`,
+    erroExcluir: (nome) => `Erro ao excluir ${nome}.`,
+    erroCarregar: (nome) => `Erro ao carregar ${nome}.`,
+    errolistar: (nome) => `Erro ao listar ${nome}.`
+};
